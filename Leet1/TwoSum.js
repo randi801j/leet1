8cc3bcaa8fbc31f7a54b57f60d2e3c813578bf4c
Randy Luc <113475637+randi801j@users.noreply.github.com>
2023-06-16 13:20:24 -0700

    Update TwoSum.js

--- Leet1/TwoSum.js
+++ Leet1/TwoSum.js
@@ -1,13 +1,21 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
-                }
-            }
-        }
-        // In case there is no solution, we'll just return null
-        return null;
+// Given an array of integers nums and an integer target, 
+//return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, 
+//and you may not use the same element twice.
+
+// You can return the answer in any order.
+var twoSum = function(nums, target) {
+  let ans = []; // to store the indices of the two numbers
+  for (let i = 0; i < nums.length; i++) { // loop through each element in the array
+    for (let j = i + 1; j < nums.length; j++) { // loop through the rest of the array to find the other number
+      let sum = nums[i] + nums[j]; // calculate the sum of the two numbers
+      if (sum === target) { // check if the sum equals the target value
+        ans.push(i); // store the indices of the two numbers
+        ans.push(j);
+        return ans; // return the answer
+      }
     }
+  }
+  return ans; // if no such numbers exist, return an empty vector
 }
